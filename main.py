ba22f7864d444586ed520b2c90cb95d9790ed31b
HackerSM9 <sm9hacking@gmail.com>
2024-05-23 16:11:33 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -21,3 +21,21 @@ elif choice == "2":
     banner.banner()
     ip_add = input("Enter IP Address: ")
     os.system("nmap {}".format(ip_add))
+elif choice == "3":
+    os.system("clear")
+    banner.banner()
+    dns = input("Enter DNS (URL/IP Add): ")
+    os.system("nmap {}".format(dns))
+elif choice == "4":
+    os.system("clear")
+    banner.banner()
+    ping = input("Enter URL/IP Address to Ping: ")
+    os.system("nmap -sP {}".format(ping))
+elif choice == "9":
+    os.system("termux-open-url https://github.com/HackerSM9/ip-tracker")
+elif choice == "x" or "X":
+    os.system("termux-open-url https://github.com/HackerSM9/")
+elif choice == "off" or "OFF":
+    sys.exit()
+else:
+    print(":( No Option from Above List. ")
