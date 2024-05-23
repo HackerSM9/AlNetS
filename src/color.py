ae9fd9301624803994dab309f87f51ccd72a88d5
HackerSM9 <sm9hacking@gmail.com>
2024-05-23 22:07:26 +0530

    new file:   src/color.py

---
+++ src/color.py
@@ -0,0 +1,7 @@
+red = '\033[1;31m'
+green = '\033[1;32m'
+yellow = '\033[1;33m'
+violet = '\033[1;34m'
+end = '\033[0m'
+question = yellow+'['+red+'?'+yellow+']'+end
+tick = yellow+'['+green+'✓'+yellow+']'+end
